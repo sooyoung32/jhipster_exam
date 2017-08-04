af829a06903e257f89562b0a23e39fb099a909bb
hkpking <hkpking@tmon.co.kr>
2017-08-04 18:06:40 +0900

    Initial commit

---
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -0,0 +1,38 @@
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+import { WindowRef } from './tracker/window.service';
+import {
+    JhipsterSharedLibsModule,
+    JhiLanguageHelper,
+    FindLanguageFromKeyPipe,
+    JhiAlertComponent,
+    JhiAlertErrorComponent
+} from './';
+
+@NgModule({
+    imports: [
+        JhipsterSharedLibsModule
+    ],
+    declarations: [
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ],
+    providers: [
+        JhiLanguageHelper,
+        WindowRef,
+        Title,
+        {
+            provide: LOCALE_ID,
+            useValue: 'ko'
+        },
+    ],
+    exports: [
+        JhipsterSharedLibsModule,
+        FindLanguageFromKeyPipe,
+        JhiAlertComponent,
+        JhiAlertErrorComponent
+    ]
+})
+export class JhipsterSharedCommonModule {}

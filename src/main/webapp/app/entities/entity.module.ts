544a60f6dd836f2af87ce7c9802b1075fc48a2f6
hkpking <hkpking@tmon.co.kr>
2017-08-18 15:15:07 +0900

    jdl import(pagination) 후 생성/변경 파일

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,11 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { JhipsterCustomerModule } from './customer/customer.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        JhipsterCustomerModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
